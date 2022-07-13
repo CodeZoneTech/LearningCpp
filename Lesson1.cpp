d7cf970919339b031f46d79289326df7e06bdc95
Pramuditha Bhanu Pothuwila <pramudithapothuwila36@gmail.com>
2022-07-13 14:02:44 +0530

    Lesson1 added
    Signed-off-by: Pramuditha Bhanu Pothuwila <pramudithapothuwila36@gmail.com>

---
+++ Lesson1.cpp
@@ -0,0 +1,16 @@
+/**
+ Lesson 1
+ C++ with CodeZone
+ 
+ Printing string to console
+ **/
+
+#include <iostream>
+
+
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;// std::endl; can be used to go to newline in the console
+    std::cout << "By \nCodeZone";// or use \n to go to a new line.
+}
