988aa6f07bc0b0606ab7c1404fbac2eec84e0c81
Pramuditha Bhanu Pothuwila <pramudithapothuwila36@gmail.com>
2022-07-15 10:23:07 +0530

    Lesson1 updated
    Signed-off-by: Pramuditha Bhanu Pothuwila <pramudithapothuwila36@gmail.com>

--- src/Lesson1.cpp
+++ src/Lesson1.cpp
@@ -8,9 +8,13 @@
 #include <iostream>
 
 
-
 int main()
 {
     std::cout << "Hello World!" << std::endl;// std::endl; can be used to go to newline in the console
     std::cout << "By \nCodeZone";// or use \n to go to a new line.
+    
+    // std::cout can be used for printing string without formatting
+    
+    char* out = "This uses printf";
+    std::printf("%s", out); // A char pointer is used to pass a string to printf
 }
