6cb4b334a359edf928b21e4a30882b897311ce30
Pramuditha Bhanu Pothuwila <pramudithapothuwila36@gmail.com>
2022-07-21 21:33:14 +0530

    Lesson3 added and is disabled at moment
    Signed-off-by: Pramuditha Bhanu Pothuwila <pramudithapothuwila36@gmail.com>

--- src/Lesson2.cpp
+++ src/Lesson2.cpp
@@ -5,19 +5,19 @@
  Variables and Functions in C++
  **/
 
-#include <iostream>
-#include <string>
-
-void printName(std::string input);// Function Declaration
-
-int main()
-{
-    std::string example;// Variable declaration
-    example = "Hello World!";// Variable assignment
-    printName(example);
-}
-
-void printName(std::string input)// Function Definition
-{
-    std::cout << input;
-}
+//#include <iostream>
+//#include <string>
+//
+//void printName(std::string input);// Function Declaration
+//
+//int main()
+//{
+//    std::string example;// Variable declaration
+//    example = "Hello World!";// Variable assignment
+//    printName(example);
+//}
+//
+//void printName(std::string input)// Function Definition
+//{
+//    std::cout << input;
+//}
