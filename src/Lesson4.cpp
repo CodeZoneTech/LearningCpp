da663ce88f14c8af86626eb3fef483645d1575b5
Pramuditha Bhanu Pothuwila <pramudithapothuwila36@gmail.com>
2022-07-21 22:01:08 +0530

    Lesson4 added and is disabled at moment
    Signed-off-by: Pramuditha Bhanu Pothuwila <pramudithapothuwila36@gmail.com>

---
+++ src/Lesson4.cpp
@@ -0,0 +1,36 @@
+/**
+ Lesson 4
+ C++ with CodeZone
+ 
+ Basic Loops in C++
+ **/
+//
+//#include <iostream>
+//
+//int main()
+//{
+//    //For Loop
+//
+//    for(int i = 0; i < 10; i++)
+//    {
+//        std::cout << "For Loop " << i << std::endl;
+//    }
+//
+//    int j = 0;
+//    //While Loop
+//
+//    while(j < 10)
+//    {
+//        std::cout << "While Loop " << j << std::endl;
+//        j++;
+//    }
+//
+//    int k = 0;
+//    //Do While Loop
+//
+//    do
+//    {
+//        std::cout << "Do While Loop " << k << std::endl;
+//        k++;
+//    } while (k < 10);
+//}
