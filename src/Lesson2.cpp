011123a7aae50a88fb06c04c19d0e01446182560
Pramuditha Bhanu Pothuwila <pramudithapothuwila36@gmail.com>
2022-07-21 21:18:00 +0530

    Lesson2 updated
    Signed-off-by: Pramuditha Bhanu Pothuwila <pramudithapothuwila36@gmail.com>

---
+++ src/Lesson2.cpp
@@ -0,0 +1,23 @@
+/**
+ Lesson 2
+ C++ with CodeZone
+ 
+ Variables and Functions in C++
+ **/
+
+#include <iostream>
+#include <string>
+
+void printName(std::string input);// Function Declaration
+
+int main()
+{
+    std::string example;// Variable declaration
+    example = "Hello World!";// Variable assignment
+    printName(example);
+}
+
+void printName(std::string input)// Function Definition
+{
+    std::cout << input;
+}
