c681920413add67744974e5fe70a527b7dd89491
Pramuditha Bhanu Pothuwila <pramudithapothuwila36@gmail.com>
2022-07-31 15:56:02 +0530

    Lesson5 updated and disabled
    Signed-off-by: Pramuditha Bhanu Pothuwila <pramudithapothuwila36@gmail.com>

--- src/Lesson5.cpp
+++ src/Lesson5.cpp
@@ -7,25 +7,39 @@
 
 #include <iostream>
 
+void returnFunction();
 
-int main()
-{
-    for(int i = 0; i < 10; i++)
-    {
-        if(i == 5)
-        {
-            continue;
-        }
-        std::cout << "Continue " << i << std::endl;
-    }
-    
-    for(int i = 0; i < 10; i++)
-    {
-        if(i == 5)
-        {
-            break;
-        }
-        std::cout << "Break " << i << std::endl;
-    }
-}
+//int main()
+//{
+//    for(int i = 0; i < 10; i++)
+//    {
+//        if(i == 5)
+//        {
+//            continue; // continue skips the current iteration. loop run to completion
+//        }
+//        std::cout << "Continue " << i << std::endl;
+//    }
+//
+//    for(int i = 0; i < 10; i++)
+//    {
+//        if(i == 5)
+//        {
+//            break;// break stops the loop and return to the loop end. loop won't run to completion
+//        }
+//        std::cout << "Break " << i << std::endl;
+//    }
+//    returnFunction();
+//}
+//
+//void returnFunction()
+//{
+//    for(int i = 0; i < 10; i++)
+//    {
+//        if(i == 5)
+//        {
+//            return;// return stops the loop and return to the loop end. loop won't run to completion
+//        } //Main use of return is for returning a value from a function;
+//        std::cout << "Return " << i << std::endl;
+//    }
+//}
 
