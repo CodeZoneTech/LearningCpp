5efe391a4c963d61ba8bb050ba61a02915ae246e
Pramuditha Bhanu Pothuwila <pramudithapothuwila36@gmail.com>
2022-07-31 22:38:58 +0530

    Lesson7 added and disabled
    Signed-off-by: Pramuditha Bhanu Pothuwila <pramudithapothuwila36@gmail.com>

---
+++ src/Lesson7.cpp
@@ -0,0 +1,22 @@
+/**
+ Lesson 7
+ C++ with CodeZone
+ 
+ References in C++
+ **/
+ 
+#include <iostream>
+
+
+//References doesn't point to a variable like pointers. References are just aliases to variables.
+
+//int main()
+//{
+//    float var = 123.321;
+//
+//    float& ref = var;
+//
+//    std::cout << var << std::endl;
+//
+//    std::cout << ref << std::endl;// ref variable is an alias to var variable
+//}
